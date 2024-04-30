4d63e3edaf59a0ac7fb979f68bf185de37f3a6c2
Bipon Roy <biponroy400@gmail.com>
2024-04-30 16:05:37 +0600

    Added pdf dowload option on payslip

--- src/components/Task/Payslip/PayslipDocument.tsx
+++ src/components/Task/Payslip/PayslipDocument.tsx
@@ -1,5 +1,5 @@
 import logo from "../../../assets/images/logo-dark.png";
-
+import "./payslip.css";
 const earningsData = [
     {
         title: "Basic Salary",
@@ -48,17 +48,17 @@ const deductionsData = [
 const PayslipDocument = () => {
     return (
         <>
-            <div className="bg-white p-4 mt-3 ">
+            <div className="bg-white p-2 p-md-4 mt-3 ">
                 <h4 className="text-decoration-underline text-center">
                     PAYSLIP FOR THE MONTH OF FEB 2024
                 </h4>
                 <div className="d-flex justify-content-between align-items-center mt-2">
                     <div>
-                        <img className="" src={logo} alt="Logo" />
+                        <img className="icon-image" src={logo} alt="Logo" />
                     </div>
                     <div>
-                        <h3>PAYSLIP #49029</h3>
-                        <h5 className="fw-bolder">Salary Month: March, 2024</h5>
+                        <h3 className="fs-4 fs-lg-3 text-end">PAYSLIP #49029</h3>
+                        <h5 className="fw-bolder fs-6 fs-lg-5">Salary Month: March, 2024</h5>
                     </div>
                 </div>
 
@@ -77,7 +77,7 @@ const PayslipDocument = () => {
 
                 <div className="container-fluid p-0 m-0">
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-md-6">
                             <h3 className="mb-1 fw-bolder">Earnings</h3>
                             {earningsData.map((d, idx) => (
                                 <article
@@ -92,7 +92,7 @@ const PayslipDocument = () => {
                                 </article>
                             ))}
                         </div>
-                        <div className="col-lg-6">
+                        <div className="col-md-6 ">
                             <h3 className="mb-1 fw-bolder">Deductions</h3>
                             {deductionsData.map((d, idx) => (
                                 <article
@@ -109,6 +109,11 @@ const PayslipDocument = () => {
                         </div>
                     </div>
                 </div>
+
+                <h4 className=" fw-m">
+                    <strong>Net Salary: $59698</strong> (Fifty nine thousand six hundred and ninety
+                    eight only.)
+                </h4>
             </div>
         </>
     );
--- src/components/Task/Payslip/PayslipInfo.tsx
+++ src/components/Task/Payslip/PayslipInfo.tsx
@@ -1,15 +1,31 @@
 import PayslipDocument from "./PayslipDocument";
+import { usePDF } from "react-to-pdf";
+import Button from "react-bootstrap/Button";
 
 const PayslipInfo = () => {
+    const { toPDF, targetRef } = usePDF({ filename: "Payslip.pdf" });
+
     return (
         <>
-            <div className="mt-2">
-                <h3 className="fw-bold" style={{ marginBottom: "1rem" }}>
-                    Payslip
-                </h3>
-                <h4 className="fw-bolder">Dashboard / Payslip</h4>
+            <div className="d-flex justify-content-between align-items-center">
+                <div className="mt-2">
+                    <h3 className="fw-bold" style={{ marginBottom: "1rem" }}>
+                        Payslip
+                    </h3>
+                    <h4 className="fw-bolder">Dashboard / Payslip</h4>
+                </div>
+
+                <Button
+                    onClick={() => toPDF()}
+                    className="mt-3 text-dark-emphasis bg-white fw-bolder "
+                    variant="light"
+                >
+                    PDF
+                </Button>
             </div>
-            <PayslipDocument />
+            <div ref={targetRef}>
+                <PayslipDocument />
+            </div>
         </>
     );
 };
